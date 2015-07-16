58bba8258510c373432afe4e10eb617f4114ce52
FranTirado <blazmist@gmail.com>
2015-07-16 11:04:54 +0200

    Update and rename Main to #createRandom.py

--- #createRandom.py
+++ #createRandom.py
@@ -1,26 +1,38 @@
+#createRandom.py
+
 import maya.cmds as cmds
 import random
 
 random.seed(1234)
 
+
+'''
+#Crea una lista con los cubos creados
 cubeList = cmds.ls('myCube*')
 if len(cubeList)>0:
     cmds.delete(cubeList)
-    
-    
+#Crea un cubo de 1x1x1 y lo nombra
 result = cmds.polyCube (w=1, h=1, d=1, name='miCubo#')
+'''
+
+#Crea conforme al orden del selección el en viewport
+result = cmds.ls(orderedSelection=True)
+
+print 'result: %s' %(result)
 
 transformName = result[0]
 
+#Crea un grupo para los cubos creados
 instanceGroupName = cmds.group( empty=True, name=transformName + '_instance_grp#')
 
+#Crea cubos como instancias de forma random
 for i in range(0, 20):
     instanceResult = cmds.instance(transformName, name=transformName + '_instance#')
     
     cmds.parent(instanceResult, instanceGroupName)
     
-    x = random.uniform(-5, 5)
-    y = random.uniform(-2, 10)
+    x = random.uniform(-10, 10)
+    y = random.uniform(-10, 10)
     z = random.uniform(-10, 10)
     
     cmds.move(x,y,z, instanceResult)
